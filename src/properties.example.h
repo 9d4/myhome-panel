17128ba9e955b20e9172b79cd1a3bab1926a6eed
traper <52043756+traperwaze@users.noreply.github.com>
2022-02-22 05:28:25 +0700

    cleaning

---
+++ src/properties.example.h
@@ -0,0 +1,10 @@
+#define BLYNK_FIRMWARE_VERSION "1.0.0"
+#define BLYNK_TEMPLATE_ID "Template ID"
+#define BLYNK_DEVICE_NAME "Device Name"
+#define BLYNK_AUTH_TOKEN "hdgfjieur7YteuUhdJkIkOkigydg48heue8e3wey"
+#define BLYNK_PRINT Serial
+
+char auth[] = BLYNK_AUTH_TOKEN;
+
+char ssid[] = "yourwifi";
+char pass[] = "yoursecret";
